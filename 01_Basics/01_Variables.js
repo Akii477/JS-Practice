d83903def17e09fa12e2ed4a1ff9d34d89f5722c
Akii477 <rajkushwaha2410@gmail.com>
2024-03-09 15:21:32 +0530

    variables

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -1,2 +1,18 @@
-let num = 10;
-console.log(num);
+const accountId = 144553;
+let accountEmail = "rajkushwaha2410@gmail.com";
+var accountPassword = "12345";
+accountCity = "Mumbai"; //Not good practice.
+let accountState;
+
+// accountId = 2; // not allowed
+accountEmail = "demo@gmail.com";
+accountPassword = "2121345";
+accountCity = "Jaipur";
+
+console.log(accountId);
+
+/* 
+    Prefer not to use VAR
+    because of issue in block scope and functional scope
+ */
+console.table( [accountEmail, accountId, accountPassword, accountCity, accountState]);
