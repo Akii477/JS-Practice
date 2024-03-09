05a15209237bcab5e346c59265cb66c5eb5d3921
Akii477 <rajkushwaha2410@gmail.com>
2024-03-09 19:23:59 +0530

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,33 @@
+// Easy to predict answers
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log( 2 != 1);
+
+
+// When we compare different data types value it will be difficult to predict
+
+console.log("2" > 1);
+console.log("02" > 1); 
+
+
+// compare to NULL
+// Avoid this type of conversion
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+/* The Reason is that am equality check == and comparision > < >= <= work differently
+    Comparision converts null to number, treating is as 0 That's why null >=0 is true and null>0 is false;
+*/
+
+
+
+// === strict check --> check data as well as data type
+console.log("2" === 2);
+
+
+
+
+
