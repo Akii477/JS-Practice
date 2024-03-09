0098bd9473e7158ac476d8aea7bf29890b6ce44d
Akii477 <rajkushwaha2410@gmail.com>
2024-03-09 15:21:46 +0530

    data_types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( "Hello" ); //We are using NodeJS not BROWSER
+
+let name = "Akash";
+let age = 21;
+let isLoggerIn = false;
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+//  symbol => when we talk about unique it is symbol
+
+
+// OBJECT
+
+console.log( typeof undefined); // undefined
+console.log(typeof null); // OBJECT
