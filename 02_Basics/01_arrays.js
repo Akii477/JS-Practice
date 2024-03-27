823b9565a93258ab25f71a60cd23a9acd5072390
Akii477 <rajkushwaha2410@gmail.com>
2024-03-27 16:58:09 +0530

    Array in Depth

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,49 @@
+// array
+// In JS array can store non similar type of element there is no rule of similar element Like C++.
+// IN JS array are re-sizable.
+// JS array-copy-operations create shallow copies. (shallow copies --> are copies which have same references to original addresses.)
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["Shaktiman","Nagraj"];
+
+console.log(myArr[1]);
+// another way to create ARRAY
+const myArr2 = new Array(1,2,3,4);
+
+
+// Array Methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+
+// myArr.unshift(9);
+// myArr.shift()
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9)); // -1 if not exists
+
+console.log(myArr);
+
+
+const newArr = myArr.join();
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
+
+
+// SLICE, SPLICE
+
+// TODO: Interview Questions : Difference Between Slice and Splice. 
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1, 3);
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+console.log("C", myArr);
+
+
+
+
+
---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,48 @@
+const marvel_heros = ["thor", "Ironman", "Spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros); // [ 'thor', 'Ironman', 'Spiderman', [ 'superman', 'flash', 'batman' ] ] Array ke ander Array.
+//Here dc_heros array is a single element in mervel_heroes array.
+// console.log(marvel_heros[3]);
+
+// for select perticular element in subarray at index 3
+// console.log(marvel_heros[3][1]);
+
+// Another Method: concate;
+let resultconcate = marvel_heros.concat(dc_heros);
+console.log(resultconcate); // concate return new array where push add element in existing array.
+// [ 'thor', 'Ironman', 'Spiderman', 'superman', 'flash', 'batman' ]
+
+// SPREAD OPERATOR:
+const all_new_heros = [...marvel_heros, ...dc_heros];
+console.log(all_new_heros);
+
+
+// FLAT: 
+const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+let usable_another_array = anotherArray.flat(Infinity);
+console.log(usable_another_array);
+
+
+// Array.from
+
+console.log(Array.isArray("Akash")); 
+
+console.log(Array.from("Akash")); 
+
+//from can not directly convert key: value pair into array we have to specify.
+console.log(Array.from({name:"Akash"})); // INTERESTING INTERVIEW PERSPECTIVE;
+
+
+//Array.of 
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
+
+
